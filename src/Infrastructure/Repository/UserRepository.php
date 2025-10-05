9c53513889cd8b0bd9b9d39d697c7737a0b04433
Sergey Petrov <cl@coders-lair.com>
2025-10-05 07:19:06 +0300

    lesson-6 Doctrine advanced practices

--- src/Infrastructure/Repository/UserRepository.php
+++ src/Infrastructure/Repository/UserRepository.php
@@ -3,6 +3,7 @@
 namespace App\Infrastructure\Repository;
 
 use App\Domain\Entity\User;
+use DateInterval;
 use Doctrine\Common\Collections\Criteria;
 use Doctrine\ORM\AbstractQuery;
 use Doctrine\ORM\NonUniqueResultException;
@@ -15,6 +16,12 @@ class UserRepository extends AbstractRepository
     public function create(User $user): int
     {
         return $this->store($user);
+    }
+
+    public function remove(User $user): void
+    {
+        $user->setDeletedAt();
+        $this->flush();
     }
 
     public function subscribeUser(User $author, User $follower): void
@@ -126,5 +133,23 @@ class UserRepository extends AbstractRepository
             ->setParameter('userId', $userId);
 
         return $queryBuilder->executeQuery()->fetchAllNumeric();
+    }
+
+    public function removeInFuture(User $user, DateInterval $dateInterval): void
+    {
+        $user->setDeletedAtInFuture($dateInterval);
+        $this->flush();
+    }
+
+    /**
+     * @return User[]
+     */
+    public function findUsersByLoginWithDeleted(string $name): array
+    {
+        $filters = $this->entityManager->getFilters();
+        if ($filters->isEnabled('soft_delete_filter')) {
+            $filters->disable('soft_delete_filter');
+        }
+        return $this->entityManager->getRepository(User::class)->findBy(['login' => $name]);
     }
 }
