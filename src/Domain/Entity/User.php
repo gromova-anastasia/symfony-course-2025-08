60c4947cfee862f89c6202f11a509df07bf1a947
Sergey Petrov <sir.scahr@gmail.com>
2025-10-23 21:13:03 +0300

    lesson-12 Stateless_API

--- src/Domain/Entity/User.php
+++ src/Domain/Entity/User.php
@@ -77,6 +77,9 @@ class User implements EntityInterface, HasMetaTimestampsInterface, SoftDeletable
     #[ORM\Column(type: 'json', length: 1024, nullable: false)]
     private array $roles = [];
 
+    #[ORM\Column(type: 'string', length: 32, unique: true, nullable: true)]
+    private ?string $token = null;
+
     public function __construct()
     {
         $this->tweets = new ArrayCollection();
@@ -273,6 +276,16 @@ class User implements EntityInterface, HasMetaTimestampsInterface, SoftDeletable
     public function setRoles(array $roles): void
     {
         $this->roles = $roles;
+    }
+
+    public function getToken(): ?string
+    {
+        return $this->token;
+    }
+
+    public function setToken(?string $token): void
+    {
+        $this->token = $token;
     }
 
     public function eraseCredentials(): void
